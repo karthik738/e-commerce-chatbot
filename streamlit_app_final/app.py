a319d37a4a68be8fda75ea4a51c4ed8e136b29b8
Karthik <kolahalkarthik@gmail.com>
2025-01-19 14:33:38 +0530

    Changes

--- streamlit_app_final/app.py
+++ streamlit_app_final/app.py
@@ -240,13 +240,13 @@ def render_query_page():
                         st.session_state["temp_query"] = ""
                         # input_placeholder.empty()  # Clear the input placeholder
                         # Re-render the chat with the updated history
-                        # display_chat()
+                        display_chat()
                     else:
                         st.error(f"Query failed. Status code: {response.status_code}")
                 except Exception as e:
                     st.error(f"An error occurred during query: {e}")
     # Display the updated chat history dynamically
-    display_chat()
+    # display_chat()
 
 
 # Clears the input but have to refresh page to get the response
